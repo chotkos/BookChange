e9172f724cc55b10cc40aac131dc5440219fc5d0
Mateusz Chodkowski <chotkos@gmail.com>
2015-05-27 13:37:19 +0200

    Initial commit - podstawowe elementy bootstrap

---
+++ bookchange/bookchange.js
@@ -0,0 +1,31 @@
+Books = new Mongo.Collection("books");
+
+if (Meteor.isClient) {
+    Meteor.subscribe("books");
+
+    // counter starts at 0
+    /*Session.setDefault('counter', 0);
+
+    Template.hello.helpers({
+      counter: function () {
+        return Session.get('counter');
+      }
+    });
+
+    Template.hello.events({
+      'click button': function () {
+        // increment the counter when button is clicked
+        Session.set('counter', Session.get('counter') + 1);
+      }
+    });*/
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
+
+    Meteor.publish("books", function () {
+        return Books.find();
+    });
+}
