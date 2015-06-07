cd7a2cc56695534aba108aebfa7610e4dad23a01
Mateusz Chodkowski <chotkos@gmail.com>
2015-06-07 18:17:19 +0200

    cookietemplate on cookie going

--- bookchange/client/Templates/cookie/cookieTemplate.js
+++ bookchange/client/Templates/cookie/cookieTemplate.js
@@ -1,14 +1,20 @@
 if (Meteor.isClient) {
     Template.cookieTemplate.helpers({
         isCookieTemplateVisible: function () {
-            var x = Session.get("cookieTemplateVisible")
-            return x != undefined ? x : true;
+            var x = localStorage.getItem("cookieTemplateVisible")
+            if (x === "false") {
+                Session.set("cookieTemplateVisible", false);
+            } else {
+                Session.set("cookieTemplateVisible", true);
+            }
+            return Session.get("cookieTemplateVisible");
         }
     });
 
     Template.cookieTemplate.events({
         'click button': function (event) {
             event.preventDefault();
+            localStorage.setItem("cookieTemplateVisible", false);
             Session.set("cookieTemplateVisible", false);
             return false;
         }
